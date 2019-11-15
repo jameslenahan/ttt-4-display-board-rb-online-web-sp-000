95384466bb7623e27677a47b0f239bd26b2d5255
James Lenahan <james.k.lenahan@gmail.com>
2019-11-15 13:47:10 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,9 +1,11 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board
-  puts"   |   |   "
+  puts" #{board[0]} | [1] | [2] "
   puts"-----------"
-  puts"   |   |   "
+  puts" [3] | [4] | [5] "
   puts"-----------"
-  puts"   |   |   "
+  puts" [6] | [7] | [8] "
 end
+
+board["", "", "", "", "", "", "", "", ""]
